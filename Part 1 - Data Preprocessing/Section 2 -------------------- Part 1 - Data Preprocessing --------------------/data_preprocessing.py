f2bbdcf226b1706e5c9c262570d7b04642850b01
Takuo Yoneda <tyoneda@cmgames.co.jp>
2017-05-25 12:40:14 +0900

    Taking care of missing data.

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-# Data Preprocessing
-
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
