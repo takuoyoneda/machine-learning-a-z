b25a97a2d91f5373f7a1be51a79e15a09d54be52
Takuo Yoneda <tyoneda@cmgames.co.jp>
2017-05-26 19:06:17 +0900

    Multiple linear regression.

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -7,14 +7,13 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values # independent variables
-y = dataset.iloc[:, 4].values # dependent variable
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
-# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
@@ -40,26 +39,3 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
-
-# Building the optimal model using Backward Elimination
-import statsmodels.formula.api as sm
-X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
-X_opt = X[:, [0,1,2,3,4,5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[:, [0,1,3,4,5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[:, [0,3,4,5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[:, [0,3,5]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
-
-X_opt = X[:, [0,3]]
-regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
