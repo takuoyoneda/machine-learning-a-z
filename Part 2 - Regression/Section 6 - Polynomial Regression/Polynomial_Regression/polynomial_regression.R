932b441b8bdd989069b794278b0edd97e250b6cc
Takuo Yoneda <tyoneda@cmgames.co.jp>
2017-05-29 15:53:26 +0900

    Finished Polynomial Regression

--- Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/polynomial_regression.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/polynomial_regression.R
@@ -5,10 +5,10 @@ dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+# install.packages('caTools')
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 # training_set = subset(dataset, split == TRUE)
 # test_set = subset(dataset, split == FALSE)
 
@@ -29,50 +29,32 @@ poly_reg = lm(formula = Salary ~ .,
 
 # Visualising the Linear Regression results
 # install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
+# library(ggplot2)
+ggplot() + 
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
+             color = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
-            colour = 'blue') +
+            color = 'blue') +
   ggtitle('Truth or Bluff (Linear Regression)') +
   xlab('Level') +
   ylab('Salary')
 
 # Visualising the Polynomial Regression results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
+ggplot() + 
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
+             color = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(poly_reg,
-                                        newdata = data.frame(Level = x_grid,
-                                                             Level2 = x_grid^2,
-                                                             Level3 = x_grid^3,
-                                                             Level4 = x_grid^4))),
-            colour = 'blue') +
+            color = 'blue') +
   ggtitle('Truth or Bluff (Polynomial Regression)') +
   xlab('Level') +
   ylab('Salary')
 
 # Predicting a new result with Linear Regression
-predict(lin_reg, data.frame(Level = 6.5))
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
 
 # Predicting a new result with Polynomial Regression
-predict(poly_reg, data.frame(Level = 6.5,
-                             Level2 = 6.5^2,
-                             Level3 = 6.5^3,
-                             Level4 = 6.5^4))
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
+
