6ab23fc198478f1de9d652835c6e1551411da1f5
Takuo Yoneda <tyoneda@cmgames.co.jp>
2017-05-29 18:57:34 +0900

    SVR

--- Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/data_preprocessing_template.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/data_preprocessing_template.R
@@ -16,13 +16,17 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the SVR to the dataset
+# install.packages('e1071')
+# library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
+# Visualising the SVR results
 # install.packages('ggplot2')
 # library(ggplot2)
 
@@ -31,20 +35,7 @@ ggplot() +
              color = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
             color = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (SVR Model)') +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-# library(ggplot2)
-
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() + 
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             color = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            color = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
